e723c25f27967079aaa03d90d3edffccad1e7a70
Zemali Okba <okbazemali@gmail.com>
2026-01-16 11:47:55 +0100

    Using the math library

--- combinations.py
+++ combinations.py
@@ -1,11 +1,7 @@
-def fact(n):
-    if n == 0 or n == 1:
-        return 1
-    else:
-        return n * fact(n-1)
+import math
 
 n = int(input("Enter total number of elements n: "))
 k = int(input("Enter number of elements to choose k: "))
 
-result = fact(n) // (fact(k) * fact(n - k))
+result = math.comb(n, k)
 print(f"Number of ways to choose {k} from {n} is: {result}")
