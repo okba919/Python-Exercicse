64e142c9bbf185fb089e605cf253391ded260b28
Zemali Okba <okbazemali@gmail.com>
2026-01-16 11:45:52 +0100

    With fact Recursive function

--- combinations.py
+++ combinations.py
@@ -1,13 +1,11 @@
 def fact(n):
-    result = 1
-    for i in range(1, n+1):
-        result *= i
-    return result
-
-def comb(n, k):
-    return fact(n) // (fact(k) * fact(n - k))
+    if n == 0 or n == 1:
+        return 1
+    else:
+        return n * fact(n-1)
 
 n = int(input("Enter total number of elements n: "))
 k = int(input("Enter number of elements to choose k: "))
 
-print("Number of ways to choose", k, "from", n, "is:", comb(n, k))
+result = fact(n) // (fact(k) * fact(n - k))
+print(f"Number of ways to choose {k} from {n} is: {result}")
